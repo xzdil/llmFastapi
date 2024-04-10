86d86045c85049e91450c93565f5234a5a18a2d7
xzdil <82775283+xzdil@users.noreply.github.com>
2024-04-10 10:44:01 +0500

    Добавил отдельные функции для эндпоинтов

--- gradio_app.py
+++ gradio_app.py
@@ -5,7 +5,7 @@ import time
 
 s = requests.Session()
 
-def get_stream(text, history):
+def get_stream_llm(text, history):
     conversation = Conversation()
     for user, bot in history:
         print(f'user: {user}')
@@ -21,18 +21,70 @@ def get_stream(text, history):
         response += line.decode('utf-8', errors='ignore')
         yield response
 
+def get_stream_query(text, history):
+    conversation = Conversation()
+    for user, bot in history:
+        print(f'user: {user}')
+        print(f'bot: {bot}')
+        conversation.add_user_message(user)
+        conversation.add_bot_message(bot)
+    conversation.add_user_message(text)
+    prompt = conversation.get_prompt()
+    url = f'http://localhost:8000/query?question={prompt}'
+    response = ''
+    for line in s.get(url, headers=None, stream=True):
+        print(line.decode('utf-8', errors='ignore'))
+        response += line.decode('utf-8', errors='ignore')
+        yield response
+        
+def get_stream_query(text, history):
+    conversation = Conversation()
+    for user, bot in history:
+        print(f'user: {user}')
+        print(f'bot: {bot}')
+        conversation.add_user_message(user)
+        conversation.add_bot_message(bot)
+    conversation.add_user_message(text)
+    prompt = conversation.get_prompt()
+    url = f'http://localhost:8000/db_agent?question={prompt}'
+    response = ''
+    for line in s.get(url, headers=None, stream=True):
+        print(line.decode('utf-8', errors='ignore'))
+        response += line.decode('utf-8', errors='ignore')
+        yield response
 
-demo = gr.ChatInterface(fn=get_stream, examples=["Что такое договор о долевом участии?",
-                                                 "Чем отличается уполномоченная компания от застройщика?",
-                                                 "Как мне себя обезопасить при покупке строящегося жилья?",
-                                                 "Где я могу найти утвержденную форму договора долевого участия?",
-                                                 "Какие действия необходимо совершить дольщику для приобретения жилья "
-                                                 "в строящемся доме?",
-                                                 "Кто гарантирует завершение строительства жилого объекта?",
-                                                 "Какие меры предпринимаются для предотвращения риска двойных продаж "
-                                                 "недвижимости?",
-                                                 "Какие договора не безопасны?"],
-                        title="ДолУчас-бот",
-                        description="Это чат-бот, у которого вы можете спросить информацию о долевом участии в "
-                                    "жилищном строительстве в РК")
+        
+def get_stream_query(text, history):
+    conversation = Conversation()
+    for user, bot in history:
+        print(f'user: {user}')
+        print(f'bot: {bot}')
+        conversation.add_user_message(user)
+        conversation.add_bot_message(bot)
+    conversation.add_user_message(text)
+    prompt = conversation.get_prompt()
+    url = f'http://localhost:8000/query?question={prompt}'
+    response = ''
+    for line in s.get(url, headers=None, stream=True):
+        print(line.decode('utf-8', errors='ignore'))
+        response += line.decode('utf-8', errors='ignore')
+        yield response
+
+def get_db_agent(text, history):
+    conversation = Conversation()
+    for user, bot in history:
+        print(f'user: {user}')
+        print(f'bot: {bot}')
+        conversation.add_user_message(user)
+        conversation.add_bot_message(bot)
+    conversation.add_user_message(text)
+    prompt = conversation.get_prompt()
+    url = f'http://localhost:8000/db_agent?question={prompt}'
+    response = s.get(url).decode('utf-8', errors='ignore')
+    print(response.text)
+    return response.text
+    
+demo = gr.ChatInterface(fn=get_stream, examples=["Сколько записей в базе?"],
+                        title="SQL бот",
+                        description="Бот-ассистент по базе данных")
 demo.launch(debug=True, share=True, server_name="0.0.0.0", server_port=8765)
