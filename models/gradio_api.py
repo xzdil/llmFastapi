9db39fe88020395a8681e05115a65f1f4e7c3605
xzdil <82775283+xzdil@users.noreply.github.com>
2024-04-15 11:06:02 +0500

    Create gradio_api.py

---
+++ models/gradio_api.py
@@ -0,0 +1,170 @@
+from typing import Any, Callable, Dict, Optional, Sequence
+
+import requests
+from llama_index.core.base.llms.types import (
+    ChatMessage,
+    ChatResponse,
+    ChatResponseGen,
+    CompletionResponse,
+    CompletionResponseGen,
+    LLMMetadata,
+)
+from llama_index.core.bridge.pydantic import Field, PrivateAttr
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core.constants import (
+    DEFAULT_CONTEXT_WINDOW,
+    DEFAULT_NUM_OUTPUTS,
+    DEFAULT_TEMPERATURE,
+)
+from llama_index.core.llms.callbacks import llm_chat_callback, llm_completion_callback
+from llama_index.core.llms.custom import CustomLLM
+from llama_index.core.base.llms.generic_utils import (
+    completion_response_to_chat_response,
+    stream_completion_response_to_chat_response,
+)
+from llama_index.core.types import BaseOutputParser, PydanticProgramMode
+from llama_index.core.utils import get_cache_dir
+from tqdm import tqdm
+
+from gradio_client import Client
+
+DEFAULT_LLAMA_CPP_MODEL_VERBOSITY = False
+class Gradio_LLM(CustomLLM):
+    model_url: Optional[str] = Field(
+        description="The URL llama-cpp model to download and use."
+    )
+    model_path: Optional[str] = Field(
+        description="The path to the llama-cpp model to use."
+    )
+    temperature: float = Field(
+        default=DEFAULT_TEMPERATURE,
+        description="The temperature to use for sampling.",
+        gte=0.0,
+        lte=1.0,
+    )
+    max_new_tokens: int = Field(
+        default=DEFAULT_NUM_OUTPUTS,
+        description="The maximum number of tokens to generate.",
+        gt=0,
+    )
+    context_window: int = Field(
+        default=DEFAULT_CONTEXT_WINDOW,
+        description="The maximum number of context tokens for the model.",
+        gt=0,
+    )
+    generate_kwargs: Dict[str, Any] = Field(
+        default_factory=dict, description="Kwargs used for generation."
+    )
+    model_kwargs: Dict[str, Any] = Field(
+        default_factory=dict, description="Kwargs used for model initialization."
+    )
+    verbose: bool = Field(
+        default=DEFAULT_LLAMA_CPP_MODEL_VERBOSITY,
+        description="Whether to print verbose output.",
+    )
+
+    _model: Any = PrivateAttr()
+
+    def __init__(
+        self,
+        model_url: Optional[str] = None,
+        model_path: Optional[str] = None,
+        temperature: float = DEFAULT_TEMPERATURE,
+        max_new_tokens: int = DEFAULT_NUM_OUTPUTS,
+        context_window: int = DEFAULT_CONTEXT_WINDOW,
+        callback_manager: Optional[CallbackManager] = None,
+        generate_kwargs: Optional[Dict[str, Any]] = None,
+        model_kwargs: Optional[Dict[str, Any]] = None,
+        verbose: bool = DEFAULT_LLAMA_CPP_MODEL_VERBOSITY,
+        system_prompt: Optional[str] = None,
+        messages_to_prompt: Optional[Callable[[Sequence[ChatMessage]], str]] = None,
+        completion_to_prompt: Optional[Callable[[str], str]] = None,
+        pydantic_program_mode: PydanticProgramMode = PydanticProgramMode.DEFAULT,
+        output_parser: Optional[BaseOutputParser] = None,
+    ) -> None:
+        model_kwargs = {
+            **{"n_ctx": context_window, "verbose": verbose},
+            **(model_kwargs or {}),  # Override defaults via model_kwargs
+        }
+
+        # check if model is cached
+        if model_path is not None:
+            self._model = Client(model_path)
+        else:
+            self._model = Client(model_path)
+
+        model_path = model_path
+        generate_kwargs = generate_kwargs or {}
+        generate_kwargs.update(
+            {"temperature": temperature, "max_tokens": max_new_tokens}
+        )
+
+        super().__init__(
+            model_path=model_path,
+            model_url=model_url,
+            temperature=temperature,
+            context_window=context_window,
+            max_new_tokens=max_new_tokens,
+            callback_manager=callback_manager,
+            generate_kwargs=generate_kwargs,
+            model_kwargs=model_kwargs,
+            verbose=verbose,
+            system_prompt=system_prompt,
+            messages_to_prompt=messages_to_prompt,
+            completion_to_prompt=completion_to_prompt,
+            pydantic_program_mode=pydantic_program_mode,
+            output_parser=output_parser,
+        )
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "GradioAPI_LLM"
+
+    @property
+    def metadata(self) -> LLMMetadata:
+        """LLM metadata."""
+        return LLMMetadata(
+            context_window=20000,
+            num_output=self.max_new_tokens,
+            model_name=self.model_path,
+        )
+
+    def _get_model_path_for_version(self) -> str:
+        """Get model path for the current llama-cpp version."""
+        return "Something gotta be here..."
+
+    @llm_chat_callback()
+    def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
+        prompt = self.messages_to_prompt(messages)
+        completion_response = self.complete(prompt, formatted=True, **kwargs)
+        return completion_response_to_chat_response(completion_response)
+
+    @llm_chat_callback()
+    def stream_chat(
+        self, messages: Sequence[ChatMessage], **kwargs: Any
+    ) -> ChatResponseGen:
+        prompt = self.messages_to_prompt(messages)
+        completion_response = self.stream_complete(prompt, formatted=True, **kwargs)
+        return stream_completion_response_to_chat_response(completion_response)
+
+    @llm_completion_callback()
+    def complete(
+        self, prompt: str, formatted: bool = False, **kwargs: Any
+    ) -> CompletionResponse:
+        response = self._model.submit(prompt, api_name="/chat")
+
+        return CompletionResponse(text=response, raw=response)
+
+    @llm_completion_callback()
+    def stream_complete(
+        self, prompt: str, formatted: bool = False, **kwargs: Any
+    ) -> CompletionResponseGen:
+        response_iter = self._model.submit(prompt, api_name="/chat")
+
+        def gen() -> CompletionResponseGen:
+            text = ""
+            for response in response_iter:
+                text += response
+                yield CompletionResponse(delta=response, text=text, raw={"hui":"hui","pizda":"pizda"})
+
+        return gen()
