ec4aeffbb0dba95a81ca5a7345daf6a1b40c2fff
xzdil <82775283+xzdil@users.noreply.github.com>
2024-06-20 01:50:33 +0500

    Update main_gradio.py

--- main_gradio.py
+++ main_gradio.py
@@ -14,7 +14,7 @@ from models.saiga_ollama import llm
 from models.gradio_api import Gradio_LLM
 from db_agent2 import sql_database
 
-from gradio_app import db_chat, doc_chat, llm_chat, main
+from gradio_app import db_chat, doc_chat, llm_chat
 
 set_global_tokenizer(
     AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-chat-hf").encode
@@ -30,7 +30,7 @@ async def lifespan(app: FastAPI):
         llms["saiga"] = llm
     else:
         llms["saiga"] = Gradio_LLM(model_path=gr_code)
-    llms["query"] = index.as_query_engine(llm=llms["saiga"],embed_model=embed_model, streaming=True, similarity_top_k=1)    
+    llms["query"] = index.as_query_engine(llm=llms["saiga"],embed_model=embed_model, streaming=True, similarity_top_k=3)    
     llms["db_agent"] = NLSQLTableQueryEngine(sql_database=sql_database, tables=["rental_portfolio"],llm=llms['saiga'])   
     yield
     llms.clear()
@@ -40,7 +40,6 @@ def run_llm(question: str) -> AsyncGenerator:
     response_iter = llm.stream_complete(question)
     for response in response_iter:
         yield response.delta
-
 
 def run_query(question: str) -> AsyncGenerator:
     query_engine = llms["query"]
@@ -69,7 +68,7 @@ async def root(question: str) -> StreamingResponse:
 
 @app.get("/db_agent")
 async def root(question: str):
-    return run_db_agent(questiom)
+    return run_db_agent(question)
 
 app = gr.mount_gradio_app(app, db_chat, path="/db_chat")
 app = gr.mount_gradio_app(app, doc_chat, path="/doc_chat")
